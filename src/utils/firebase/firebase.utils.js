8fe2ec62d0cd7219845ab619653fe5b54d47a659
lowkey-ronaldo <poma.ronaldo96@gmail.com>
2022-06-27 17:18:04 +0200

    modificato db firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,62 +1,63 @@
-// import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 
-// import {
-//     getAuth,
-//     signInWithRedirect,
-//     signInWithPopup,
-//     GoogleAuthProvider
-// } from 'firebase/auth';
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider
+} from 'firebase/auth';
 
-// import {
-//     getFirestore,
-//     doc,
-//     getDoc,
-//     setDoc
-// } from 'firebase/firestore';
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore';
 
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCu8xKUBlv20gXPZMsgHqeZnvAjfa-bLcc",
-//     authDomain: "crwn-clothing-db-6b0b7.firebaseapp.com",
-//     projectId: "crwn-clothing-db-6b0b7",
-//     storageBucket: "crwn-clothing-db-6b0b7.appspot.com",
-//     messagingSenderId: "276502202270",
-//     appId: "1:276502202270:web:c164d63eb76f32c428788f"
-// };
+const firebaseConfig = {
+    apiKey: "AIzaSyAVtNEEPdyMhIYK1Elvah1cvFG0RdZnv_I",
+    authDomain: "e-commerce-2022-ccd1f.firebaseapp.com",
+    projectId: "e-commerce-2022-ccd1f",
+    storageBucket: "e-commerce-2022-ccd1f.appspot.com",
+    messagingSenderId: "188811102920",
+    appId: "1:188811102920:web:d5bdbd61ff8dcd024d33fd"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
 
-// const firebaseApp = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
 
-// const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: 'select_account',
+});
 
-// provider.setCustomParameters({
-//     prompt: 'select_account',
-// });
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
-// export const auth = getAuth();
-// export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
 
-// export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
 
-// export const createUserDocumentFromAuth = async (userAuth) => {
-//     const userDocRef = doc(db, 'users', userAuth.uid);
+    const userSnapshot = await getDoc(userDocRef);
 
-//     const userSnapshot = await getDoc(userDocRef);
+    if(!userSnapshot.exists()){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
 
-//     if(!userSnapshot.exists()){
-//         const {displayName, email} = userAuth;
-//         const createdAt = new Date();
+        try{
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        }catch(error){
+            console.log('error creating the user', error.message);
+        }
+    }
 
-//         try{
-//             await setDoc(userDocRef, {
-//                 displayName,
-//                 email,
-//                 createdAt
-//             });
-//         }catch(error){
-//             console.log('error creating the user', error.message);
-//         }
-//     }
-
-//     return userDocRef;
-// }
+    return userDocRef;
+}
 
