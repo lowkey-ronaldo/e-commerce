e8913c64f4d052a8b508a6dc2729942b25f066fb
Ronaldo Poma <96665714+lowkey-ronaldo@users.noreply.github.com>
2022-06-28 17:28:41 +0200

    Delete src directory

--- src/components/button/button.component.jsx
+++
@@ -1,9 +0,0 @@
-// import "./button.styles.scss";
-
-// const Button = () => {
-//   return(
-//     <button className="button-container inverted" >Add to cart</button>
-//   )
-// }
-
-// export default Button;
