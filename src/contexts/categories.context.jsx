d6e058415e3931e4b12a2b3bf3a369906d5c56c2
lowkey-ronaldo <poma.ronaldo96@gmail.com>
2022-07-01 17:44:07 +0200

    Cambiato product.context in categories.context

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState, useEffect } from "react";
+
+
+export const CategoriesContext = createContext({
+    categoriesMap: [],
+});
+
+export const CategoriesProvider = ({children}) => {
+
+    const [fetchProducts, setFetchProducts] = useState({});
+    const [categoriesMap, setCategories] = useState(fetchProducts);
+    
+    const url =
+      "https://e-commerce-titoli-default-rtdb.europe-west1.firebasedatabase.app/categories.json";
+  
+    useEffect(() => {
+      fetch(url)
+        .then((resp) => resp.json())
+        .then((PRODUCTS) => setFetchProducts(PRODUCTS));
+    }, []);
+  
+    useEffect(() => {
+      setCategories(fetchProducts);
+    }, [fetchProducts]);
+
+
+    const value = {categoriesMap};
+
+    return(
+        <CategoriesContext.Provider value={value}>
+            {children}
+        </CategoriesContext.Provider>
+    )
+}
