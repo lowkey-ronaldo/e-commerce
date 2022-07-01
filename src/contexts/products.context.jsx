73ac8a802e7b3b9b91a21e354fdedea126c4f88e
lowkey-ronaldo <poma.ronaldo96@gmail.com>
2022-07-01 15:42:09 +0200

    Fetch nel product.context

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import PRODUCTS from '../shop-data.json';
 
 
@@ -7,7 +7,22 @@ export const ProductsContext = createContext({
 });
 
 export const ProductsProvider = ({children}) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    // const [products, setProducts] = useState(PRODUCTS);
+
+    const [fetchProducts, setFetchProducts] = useState([]);
+    const [products, setProducts] = useState(fetchProducts);
+    const url =
+      "https://e-commerce-luglio-2022-default-rtdb.europe-west1.firebasedatabase.app/product.json";
+  
+    useEffect(() => {
+      fetch(url)
+        .then((resp) => resp.json())
+        .then((PRODUCTS) => setFetchProducts(PRODUCTS));
+    }, []);
+  
+    useEffect(() => {
+      setProducts(fetchProducts);
+    }, [fetchProducts]);
 
 
     const value = {products};
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,20 +4,23 @@ import ProductCard from "../../components/product-card/product-card.component";
 import "./shop.styles.scss";
 
 const Shop = () => {
-  const [fetchProducts, setFetchProducts] = useState([]);
-  const [products, setProducts] = useState(fetchProducts);
-  const url =
-    "https://e-commerce-luglio-2022-default-rtdb.europe-west1.firebasedatabase.app/product.json";
 
-  useEffect(() => {
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((PRODUCTS) => setFetchProducts(PRODUCTS));
-  }, []);
+  const {products} = useContext(ProductsContext);
 
-  useEffect(() => {
-    setProducts(fetchProducts);
-  }, [fetchProducts]);
+  // const [fetchProducts, setFetchProducts] = useState([]);
+  // const [products, setProducts] = useState(fetchProducts);
+  // const url =
+  //   "https://e-commerce-luglio-2022-default-rtdb.europe-west1.firebasedatabase.app/product.json";
+
+  // useEffect(() => {
+  //   fetch(url)
+  //     .then((resp) => resp.json())
+  //     .then((PRODUCTS) => setFetchProducts(PRODUCTS));
+  // }, []);
+
+  // useEffect(() => {
+  //   setProducts(fetchProducts);
+  // }, [fetchProducts]);
 
   return (
     <div className="products-container">
